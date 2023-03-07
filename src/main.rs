4834272e4fa7ddc1cad9e2e15e7a2a428b6d836d
Ethan Zhang <ethan.uni@gmx.com>
2023-03-07 21:27:21 +0100

    Use set_times from nightly channel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(file_set_times)]
+
 extern crate nanoid;
 extern crate structopt;
 extern crate sys_mount;
@@ -246,7 +248,8 @@ mod test {
         mount, remount, unmount, MountConfig, RemountConfig, UnmountConfig, BASE_RAMDISK_FOLDER,
     };
     use std::env::temp_dir;
-    use std::fs::remove_dir_all;
+    use std::fs::{remove_dir_all, self, FileTimes, File};
+    use std::path::{Path, PathBuf};
 
     #[test]
     fn test_tmpfs() {
@@ -279,5 +282,66 @@ mod test {
         })
         .expect("Failed to unmount test tmpfs");
         assert!(!target.exists());
+    }
+
+    pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+        let mut stack = Vec::new();
+        stack.push(PathBuf::from(from.as_ref()));
+
+        let output_root = PathBuf::from(to.as_ref());
+        let input_root = PathBuf::from(from.as_ref()).components().count();
+
+        while let Some(working_path) = stack.pop() {
+            println!("process: {:?}", &working_path);
+
+            // Generate a relative path
+            let src: PathBuf = working_path.components().skip(input_root).collect();
+
+            // Create a destination if missing
+            let dest = if src.components().count() == 0 {
+                output_root.clone()
+            } else {
+                output_root.join(&src)
+            };
+            if fs::metadata(&dest).is_err() {
+                println!(" mkdir: {:?}", dest);
+                fs::create_dir_all(&dest)?;
+            }
+
+            for entry in fs::read_dir(working_path)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_dir() {
+                    stack.push(path);
+                } else {
+                    match path.file_name() {
+                        Some(filename) => {
+                            let dest_path = dest.join(filename);
+                            println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                            fs::copy(&path, &dest_path)?;
+
+                            let src_meta = fs::metadata(&path)?;
+                            let dest_meta = File::options().write(true).open(&dest_path)?;
+                            let times = FileTimes::new()
+                                .set_accessed(src_meta.accessed().unwrap())
+                                .set_modified(src_meta.modified().unwrap());
+                            dest_meta.set_times(times)?;
+                        }
+                        None => {
+                            println!("failed: {:?}", path);
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_set_modified() {
+        let src = PathBuf::from("/dev/shm/targetHEZ4FAC-MbjM0qJjFnRQp/");
+        let dest = PathBuf::from("/home/ethan/ramdisk/cargo-ramdisk/target2");
+        copy(src, dest).expect("Failed to copy");
     }
 }
